beb5b079ec76ce53e671b6ec2298c10bc2f4e910
hayamizhin <max9173627>
2019-11-07 14:03:26 +0900

    [add]post,userのmodel,view,controller作成、

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:index, :show]
+  resources :posts, only: [:index, :show, :create]
+
+  root 'posts#index'
+
 end
